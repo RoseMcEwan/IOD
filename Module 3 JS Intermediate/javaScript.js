f7da1dfb74921160404f686f5712b2f68e188397
Rose McEwan <rosemc867@yahoo.co.nz>
2026-06-15 19:52:52 +1200

    Figma

---
+++ Module 3 JS Intermediate/javaScript.js
@@ -0,0 +1,443 @@
+/*1. Create a function that takes a string as a parameter and returns the string with the first
+character of each word changed into a capital letter, as in the example below. Test it with
+different strings.*/
+const ucFirstLetters = (stringCap) => {
+    return stringCap
+    .split(' ')
+    .map(word => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+}
+console.log(ucFirstLetters("los angeles") ); //Los Angeles
+
+/*2. Create a function truncate(str, max) that truncates a given string of text if its total
+length is greater than the max length. It should return either the truncated text, with an
+ellipsis (...) added to the end if it was too long, or the original text otherwise.
+b) Write another variant of the truncate function that uses a conditional operator.*/
+
+const truncate = (str, max) => {
+    if (str.length > max) {
+        return str.slice(0, max) + "...";
+    } else {
+        return str;
+    }
+}
+
+console.log(truncate('This text will be truncated if it is too long', 25))
+// This text will be truncat...
+
+/*3. Use the following animals array for the below tasks. Test each one by printing the result to
+the console. Review the following link for tips:
+
+https://developer.mozilla.org/en-
+US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+a) Add 2 new values to the end
+b) Add 2 new values to the beginning
+c) Sort the values alphabetically
+d) Write a function replaceMiddleAnimal(newValue) that replaces the value in the
+middle of the animals array with newValue
+e) Write a function findMatchingAnimals(beginsWith) that returns a new array
+containing all the animals that begin with the beginsWith string. Try to make it work
+regardless of upper/lower case.*/
+
+const animals = ['Tiger', 'Giraffe'];
+
+animals.push('Elephant');
+animals.push('Bird');
+animals.unshift('Horse');
+animals.unshift('Bear');
+
+console.log(animals.sort());
+
+const replaceMiddleAnimal = (newValue) => {
+    animals.splice(3, 1, newValue);
+};
+
+replaceMiddleAnimal("Lion");
+
+const findMatchingAnimals = (beginsWith) => {
+    return animals.filter(animal => 
+        animal.toLowerCase().startsWith(beginsWith.toLowerCase()));
+};
+console.log(findMatchingAnimals('b'));
+
+console.log(animals);
+
+/*4. Write a function camelCase(cssProp) that changes dash-separated CSS properties like
+'margin-left' into camel-cased 'marginLeft'.
+The function should remove all dashes, and uppercase the first letter of each word after a
+dash.
+b) Create variants of the camelCase function that use different types of for loops, and
+c) with and without the conditional operator.*/
+
+const camelCase = (cssProp) => {
+    const words = cssProp.split ('-');
+
+    for (let i = 1; i < words.length; i++) { 
+        words[i] = 
+        words[i][0].toUpperCase() + 
+        words[i].slice(1); 
+    }
+    return words.join ('');
+}
+console.log(camelCase('margin-left')) // marginLeft 
+console.log(camelCase('background-image')) // backgroundImage 
+console.log(camelCase('display')) // display
+console.log(camelCase('display-this-image-well'))
+
+const camelCaseOf = (cssProp) => {
+    const words = cssProp.split ('-');
+    let result = words[0];
+        for (const word of words.slice(1)) {
+        result +=
+            word[0].toUpperCase() +
+            word.slice(1);
+    }
+    return result;
+}
+console.log(camelCaseOf('margin-left')) // marginLeft 
+console.log(camelCaseOf('background-image')) // backgroundImage 
+console.log(camelCaseOf('display')) // display
+console.log(camelCaseOf('display-this-image-well'))
+
+const camelCaseConditional = (cssProp) => {
+    return cssProp
+    .split('-')
+    .map((word, index) =>
+        index === 0
+            ? word
+            : word[0].toUpperCase() + word.slice(1)
+        )
+        .join('');
+};
+
+console.log(camelCaseConditional('margin-left')) // marginLeft 
+console.log(camelCaseConditional('background-image')) // backgroundImage 
+console.log(camelCaseConditional('display')) // display
+console.log(camelCaseConditional('display-this-image-well'));
+
+/*5. Decimal number operations in JavaScript can lead to unexpected results, as in the
+following:*/
+
+let twentyCents = 0.20
+let tenCents = 0.10
+console.log(`${twentyCents} + ${tenCents} = ${twentyCents + tenCents}`)
+// 0.2 + 0.1 = 0.30000000000000004
+
+/*We can sometimes avoid this using the toFixed function to force the number of decimal
+places as below, but it’s not always useful:*/
+let fixedTwenty = twentyCents.toFixed(2);
+let fixedTen = tenCents.toFixed(2);
+
+console.log(fixedTwenty + fixedTen);
+
+/*a) Explain why the above code returns the wrong answer: Becuase .toFixed turned into strings instead of numbers
+b) Create a function currencyAddition(float1, float2) which safely adds the two
+decimal numbers float1 and float2 and returns the correct float result.*/
+
+const currencyAddition = (float1, float2) => {
+const rounded1 = Math.round (float1 * 100);
+const rounded2 = Math.round (float2 * 100);
+  return (rounded1 + rounded2) / 100;
+};
+console.log(currencyAddition(0.1, 0.2));
+console.log(0.3 == currencyAddition(0.1, 0.2));
+
+/*c) Create a function currencyOperation(float1, float2, operation) which
+safely performs the given operation (either +, -, / or *) on the two numbers and returns the correct float result.*/
+
+const currencyOperation = (float1, float2, operation) => {
+    const num1 = float1 * 100;
+    const num2 = float2 * 100;
+
+    switch (operation) {
+        case '+':
+            return (num1 + num2) / 100;
+
+        case '-':
+            return (num1 - num2) / 100;
+
+        case '*':
+            return (num1 * num2) / 10000;
+
+        case '/':
+            return num1 / num2;
+
+        default:
+            return 'Invalid operation';
+    }
+};
+
+console.log(currencyOperation(0.1, 0.2, '+'));
+console.log(currencyOperation(0.3, 0.1, '-'));
+console.log(currencyOperation(0.2, 0.1, '*'));
+console.log(currencyOperation(0.2, 0.1, '/'));
+
+console.log(0.3 == currencyOperation(0.1, 0.2, '+'));
+
+/*d) (Extension) Extend the above function to include a fourth argument numDecimals
+which allows the operation to support different amounts of decimal places from 1 to 10.
+HINT: Assume 2 decimal places for b) and c) and use a multiplication factor. Test with
+different values as well as the below:*/
+
+const currencyOperationDecimals = (float1, float2, operation, numDecimals = 2) => {
+    const factor = 10 ** numDecimals;
+
+    const num1 = float1 * factor;
+    const num2 = float2 * factor;
+
+    let result;
+
+    switch (operation) {
+        case '+':
+            result = (num1 + num2) / factor;
+            break;
+
+        case '-':
+            result = (num1 - num2) / factor;
+            break;
+
+        case '*':
+            result = (num1 * num2) / (factor * factor);
+            break;
+
+        case '/':
+            result = num1 / num2;
+            break;
+
+        default:
+            return 'Invalid operation';
+    }
+
+    return Number(result.toFixed(numDecimals));
+};
+
+console.log(currencyOperationDecimals(0.1, 0.2, '+', 2));
+console.log(currencyOperationDecimals(0.333, 0.222, '+', 3));
+console.log(currencyOperationDecimals(0.5555, 0.1111, '-', 4));
+
+/*6. Create a function unique(duplicatesArray) which takes an array parameter that may
+include duplicates. Your function should return an array containing only the unique values
+from duplicatesArray.
+Test with the following arrays and create another one of your own.*/
+
+const unique = (duplicatesArray) => {
+   return [...new Set(duplicatesArray)];
+};
+
+const colours = ['red', 'green', 'blue', 'yellow', 'orange', 'red', 'blue', 'yellow'];
+const testScores = [55, 84, 97, 63, 55, 32, 84, 91, 55, 43];
+
+console.log(unique(colours));
+console.log(unique(testScores));
+
+/*7. Use the following array of book objects to practice the array functions for map, find and
+filter. Test each of your answers to the below tasks.
+
+a) Write a function getBookTitle(bookId) that uses the find function to return the
+title of the book object with the matching id.
+b) Write a function getOldBooks() that uses the filter function to return all book
+objects written before 1950.
+c) Write a function addGenre() that uses the map function to add a new genre property
+to all of the above books, with the value ‘classic’.
+d) (Extension) Write a function getTitles(authorInitial) that uses map and
+filter together to return an array of book titles for books written by authors whose
+names start with authorInitial.
+e) (Extension) Write a function latestBook() that uses find and forEach to get the
+book with the most recent publication date.*/
+
+//Question a
+const books = [
+    { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925 },
+    { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960 },
+    { id: 3, title: '1984', author: 'George Orwell', year: 1949 },
+    { id: 4, title: 'Brave New World', author: 'Aldous Huxley', year: 1932 },
+    { id: 5, title: 'The Catcher in the Rye', author: 'J.D. Salinger', year: 1951 }
+];
+
+const getBookTitle = (bookId) => {
+    const book = books.find(book => book.id === bookId);
+    return book.title;
+};
+
+console.log(getBookTitle(1));
+console.log(getBookTitle(3));
+
+//Question b
+const books1 = [
+    { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925 },
+    { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960 },
+    { id: 3, title: '1984', author: 'George Orwell', year: 1949 },
+    { id: 4, title: 'Brave New World', author: 'Aldous Huxley', year: 1932 },
+    { id: 5, title: 'The Catcher in the Rye', author: 'J.D. Salinger', year: 1951 }
+];
+
+const getOldBooks = () => {
+    return books1.filter(book => book.year < 1950);
+};
+
+console.log(getOldBooks());
+
+//Question C
+
+const books2 = [
+    { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925 },
+    { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960 },
+    { id: 3, title: '1984', author: 'George Orwell', year: 1949 },
+    { id: 4, title: 'Brave New World', author: 'Aldous Huxley', year: 1932 },
+    { id: 5, title: 'The Catcher in the Rye', author: 'J.D. Salinger', year: 1951 }
+];
+
+const addGenre = () => {
+    return books2.map(book =>  ({
+        ...book, 
+        genre: 'classic'}));
+};
+
+const booksWithGenre = addGenre();
+console.log(booksWithGenre);
+
+//Question D.  
+
+const books3 = [
+    { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925 },
+    { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960 },
+    { id: 3, title: '1984', author: 'George Orwell', year: 1949 },
+    { id: 4, title: 'Brave New World', author: 'Aldous Huxley', year: 1932 },
+    { id: 5, title: 'The Catcher in the Rye', author: 'J.D. Salinger', year: 1951 }
+];
+
+const latestBook = () => {
+    let latestYear = 0;
+
+    books3.forEach(book => {
+        if (book.year > latestYear) {
+            latestYear = book.year;
+        }
+    });
+
+    return books3.find(book => book.year === latestYear);
+};
+
+console.log(latestBook());
+
+/*8. The following code creates a new Map object for storing names beginning with A, B, or C
+with their phone numbers.
+a) Create a new phoneBookDEF Map to store names beginning with D, E or F
+b) Initialise the contents of phoneBookDEF by passing in an array of keys/values
+c) Update the phone number for Caroline
+d) Write a function printPhoneBook(contacts) that prints the names and phone
+numbers in the given Map
+e) Combine the contents of the two individual Maps into a single phoneBook Map
+f) Print out the full list of names in the combined phone book*/
+
+const phoneBookABC = new Map() //an empty map to begin with
+
+phoneBookABC.set('Annabelle', '0412312343')
+phoneBookABC.set('Barry', '0433221117')
+phoneBookABC.set('Caroline', '0455221182')
+
+const phoneBookDEF = new Map([
+    ['Danielle', '0411111111'],
+    ['Eva', '0422222222'],
+    ['Fred', '0433333333']
+]);
+phoneBookDEF.set ('Caroline', '02222222')
+
+console.log (phoneBookDEF);
+
+const printPhoneBook = (contacts) => {
+    contacts.forEach((phoneNumber, name) => {
+        console.log (`${name}: ${phoneNumber}`);
+    });
+};
+printPhoneBook(phoneBookABC);
+printPhoneBook(phoneBookDEF);
+
+const phoneBookFull = new Map([
+    ...phoneBookABC,
+    ...phoneBookDEF
+]);
+printPhoneBook(phoneBookFull);
+
+/*9. Given the below salaries object, perform the following tasks.
+a) Write a function sumSalaries(salaries) that calculates and returns the total of all salaries
+b) Write a function topEarner(salaries) that calculates and returns the name of the person
+earning the highest salary*/
+
+
+let salaries1 = {
+"Timothy" : 35000,
+"David" : 25000,
+"Mary" : 55000,
+"Christina" : 75000,
+"James" : 43000
+};
+
+let total = 0;
+
+const sumSalaries = (salaries) => {
+    for(let value in salaries) {
+    total += salaries[value];
+    };
+return total;
+};
+
+console.log(sumSalaries(salaries1));
+
+const topEarner = (salaries) =>  {
+    let highSalary = 0;
+    let highEarner = ''; 
+  
+    for(let value1 in salaries) {
+    if (salaries[value1] > highSalary) {
+        highSalary = salaries[value1];
+        highEarner = value1;
+    }
+}
+return highEarner;
+}
+console.log(topEarner(salaries1));
+
+/*10.The following code uses the Date object to print the current time and the number of hours
+that have passed today so far. Extend the code to do the following:
+a) Print the total number of minutes that have passed so far today
+b) Print the total number of seconds that have passed so far today
+c) Calculate and print your age as: 'I am x years, y months and z days old'
+d) Write a function daysInBetween(date1, date2) which calculates and returns the amount
+of days in between the two given dates.*/
+
+const today = new Date();
+console.log('Current time is ' + today.toLocaleTimeString())
+console.log(today.getHours()+ ' hours have passed so far today')
+console.log((today.getMinutes() + (today.getHours()) * 60) + ' Minutes have passed so far today')
+
+const birthDate = new Date('1997-07-18'); // Replace with your birthday
+const today1 = new Date();
+
+let years = today1.getFullYear() - birthDate.getFullYear();
+let months = today1.getMonth() - birthDate.getMonth();
+let days = today1.getDate() - birthDate.getDate();
+
+if (days < 0) {
+    months--;
+    days += 30; // Approx
+}
+
+if (months < 0) {
+    years--;
+    months += 12;
+}
+
+console.log(`I am ${years} years, ${months} months and ${days} days old`);
+
+const daysInBetween = (date1, date2) => {
+    const difference = date2 - date1;
+
+    return difference / (1000 * 60 * 60 * 24);
+};
+
+const date1 = new Date('1997-07-18');
+const date2 = new Date('2025-07-19');
+
+console.log(daysInBetween(date1, date2));
